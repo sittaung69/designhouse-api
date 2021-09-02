d652e96875e6ca66acbaee44c3a5c0b28fb527e9
Sitt Aung <sittaung2021@gmail.com>
2021-09-02 23:50:43 +0630

    User Register - setting up the registration fields

--- routes/api.php
+++ routes/api.php
@@ -1,2 +1,15 @@
 <?php
 
+// Public routes
+
+// Route group for authenticated users only
+Route::group(['middleware' => ['auth:api']], function () {
+
+});
+
+
+
+// Routes for guests only
+Route::group(['middleware' => ['guest:api']], function () {
+    Route::post('register', 'Auth\RegisterController@register');
+});
