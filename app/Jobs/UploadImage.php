d264d9420c9e64ec5e29dfac2604918fa51ca893
Sitt Aung <sittaung2021@gmail.com>
2021-09-05 16:41:33 +0630

    Resizing uploaded images

--- app/Jobs/UploadImage.php
+++ app/Jobs/UploadImage.php
@@ -2,6 +2,8 @@
 
 namespace App\Jobs;
 
+use Image;
+use App\Models\Design;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -12,14 +14,16 @@ class UploadImage implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    protected $design;
+
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(Design $design)
     {
-        //
+        $this->design = $design;
     }
 
     /**
@@ -29,6 +33,29 @@ class UploadImage implements ShouldQueue
      */
     public function handle()
     {
-        //
+        $disk = $this->design->disk;
+        $original_file = storage_path() . '/uploads/original/' . $this->design->image;
+
+        try {
+            // create the large imageg and save to tmp disk
+            Image::make($original_file)
+                ->fit(800, 600, function($contraint) {
+                    $contraint->aspectRatio();
+                })
+                ->save($large = storage_path('uploads/large/' . $this->design->image));
+            
+            // create the thumbnail image
+            Image::make($original_file)
+                ->fit(250, 200, function($contraint) {
+                    $contraint->aspectRatio();
+                })
+                ->save($large = storage_path('uploads/thumbnail/' . $this->design->image));
+            
+            // store images to permanent disk
+            
+        } catch (\Exception $e) {
+            \Log::error($e->getMessage());
+        }
+
     }
 }
